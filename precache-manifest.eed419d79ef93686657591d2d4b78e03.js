62702dcae7f03a243aeffd0e8d12c07a01574393
prabaharanbalaji16 <prabaharanbalaji16@gmail.com>
2020-10-22 13:27:06 +0530

    Updates

---
+++ precache-manifest.eed419d79ef93686657591d2d4b78e03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979872ebf1f7bd312fa023f263c7c56f",
+    "url": "/stopwatch/index.html"
+  },
+  {
+    "revision": "d8bd4ee9377b1f0bc468",
+    "url": "/stopwatch/static/css/main.c6a65535.chunk.css"
+  },
+  {
+    "revision": "72fd28a3ffcc46e46cdc",
+    "url": "/stopwatch/static/js/2.6a608442.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/stopwatch/static/js/2.6a608442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8bd4ee9377b1f0bc468",
+    "url": "/stopwatch/static/js/main.f566d9dc.chunk.js"
+  },
+  {
+    "revision": "cb2bcdb37a81d6e34a23",
+    "url": "/stopwatch/static/js/runtime-main.00413f5f.js"
+  },
+  {
+    "revision": "0c6b509042a7c524c2f127b2a7751fe3",
+    "url": "/stopwatch/static/media/mobile.0c6b5090.jpg"
+  }
+]);
